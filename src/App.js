cd6b6518de1d9874806c0a0f83eebc4b3aa7458c
cemek7 <106226729+cemek7@users.noreply.github.com>
2024-04-05 13:39:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
 import PropertyPage from './pages/PropertyPage';
+import TenantPage from './pages/TenantPage';
+import TenantDetailsPage from './pages/TenantDetailsPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -16,6 +18,8 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/property/:id" element={<PropertyPage />} />
+        <Route path="/tenants" exact component={TenantPage} />
+        <Route path="/tenant/:id" component={TenantDetailsPage} />
       </Routes>
       <Footer />
     </Router>
