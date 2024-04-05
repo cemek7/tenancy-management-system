4e02013afd2f068c60a1ad7e9167eebb7e533182
cemek7 <106226729+cemek7@users.noreply.github.com>
2024-04-05 13:08:35 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+function Footer() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <footer>
+      <p>&copy; 2024 Tenancy Management System</p>
+    </footer>
   );
 }
 
-export default App;
+export default Footer;
