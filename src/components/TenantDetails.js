893a62c00dacc69fb4f527550e6458be859d72a8
cemek7 <106226729+cemek7@users.noreply.github.com>
2024-04-05 13:18:05 +0100

    Update TenantDetails.js

--- src/components/TenantDetails.js
+++ src/components/TenantDetails.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+function TenantDetails({ tenant }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>{tenant.name}</h3>
+      <p>Email: {tenant.email}</p>
+      <p>Phone: {tenant.phone}</p>
+      {/* Additional tenant details */}
     </div>
   );
 }
 
-export default App;
+export default TenantDetails;
